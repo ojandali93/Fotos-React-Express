edae874c0706760d93f79d78548476ad41c66fad
OmarJ <omarjandali93@gmail.com>
2021-12-20 14:10:05 -0800

    Added the users models. Created credentials and profile.

---
+++ backend/models/user models/Credentials.js
@@ -0,0 +1,75 @@
+const sequelize = require('sequelize');
+const database = require('../database.js')
+
+const Credential = database.define(
+  'credential',
+  {
+    id: {
+      type: sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: sequelize.STRING,
+      allowNull: false,
+      validate: {
+        is: ["^[a-z]+$",'_'],
+        min: 4,
+        max: 22,
+      }
+    },
+    password: {
+      type: sequelize.STRING,
+      allowNull: false,
+      validate: {
+        min: 6,
+        max: 16
+      }
+    },
+    account_type: {
+      type: sequelize.STRING,
+      allowNull: false,
+      validate: {
+        isAlpha: true,
+        isIn: [['P', 'E', 'D', 'O']]
+      }
+    },
+    account_status: {
+      type: sequelize.STRING,
+      allowNull: false,
+      validate: {
+        isAlpha: true,
+        isIn: [['Active', 'Deactivated']]
+      }
+    },
+    account_tier: {
+      type: sequelize.STRING,
+      allowNull: false,
+      validate: {
+        isAlphaNumeric: true,
+        isIn: [['tier1', 'tier2', 'tier3']]
+      }
+    },
+    reference_id: {
+      type: sequelize.STRING,
+      allowNull: false,
+      validate: {
+        isAlphaNumeric: true,
+      }
+    },
+  },
+  {
+    createdAt: sequelize.DATE,
+    updatedAt: sequelize.DATE
+  }
+)
+
+Credential.sync()
+  .then((data) => {                                                                    
+    console.log('Credentials model has been synced')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
+
+module.exports = Credentials
---
+++ backend/models/user models/Profiles.js
@@ -0,0 +1,128 @@
+const sequelize = require('sequelize');
+const database = require('../database.js');
+const sequelize_file = requre('sequelize-file');
+
+const profile_picture = sequelize_file({
+  attribute: 'profile_picture',
+  mimetype: /^image/,
+  crop: true,
+  sizes: {
+    small: 64, 
+    big: 128, 
+    large: 256,
+    x_large: 512
+  }
+});
+
+const Profile = new sequelize.define(
+  'profile',
+  {
+    id: {
+      type: sequelize.INTEGER, 
+      autoIncrement: true, 
+      primaryKey: true,
+    },
+    profile_pic: {
+
+    },
+    bio: {
+      type: sequelize.STRING,
+      allowNull: true,
+      validation: {
+        max:200
+      }
+    },
+    location: {
+      type: sequelize.STRING,
+      allowNull: true,
+      validate: {
+        isAlpha: true,
+        max: 36
+      }
+    },
+    sm_facebook: {
+      type: sequelize.STRING,
+      allowNull: true,
+      validate: {
+        isUrl: true,
+      }
+    },
+    sm_instagram: {
+      type: sequelize.STRING,
+      allowNull: true,
+      validate: {
+        isUrl: true,
+      }
+    },
+    sm_twitter: {
+      type: sequelize.STRING,
+      allowNull: true,
+      validate: {
+        isUrl: true,
+      }
+    },
+    sm_website: {
+      type: sequelize.STRING,
+      allowNull: true,
+      validate: {
+        isUrl: true,
+      }
+    },
+    followers: {
+      type: sequelize.INTEGER,
+      allowNull: false,
+      default: 0,
+      validate: {
+        isInt: true,
+      }
+    },
+    following: {
+      type: sequelize.INTEGER,
+      allowNull: false,
+      default: 0,
+      validate: {
+        isInt: true,
+      }
+    },
+    photos: {
+      type: sequelize.INTEGER,
+      allowNull: false,
+      default: 0,
+      validate: {
+        isInt: true,
+      }
+    },
+    downloads: {
+      type: sequelize.INTEGER,
+      allowNull: false,
+      default: 0,
+      validate: {
+        isInt: true,
+      }
+    },
+    edits: {
+      type: sequelize.INTEGER,
+      allowNull: false,
+      default: 0,
+      validate: {
+        isInt: true,
+      }
+    }
+  },
+  {
+    createdAt: sequelize.DATE,
+    updatedAt: sequelize.DATE
+  }
+) 
+
+profile_picture.addTo(Profile);
+
+Profile.sync()
+  .then((data) => {
+    console.log('Profile model has been synced')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
+
+module.exports = Profile
